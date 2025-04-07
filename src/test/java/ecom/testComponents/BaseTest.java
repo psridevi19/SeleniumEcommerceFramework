e4405212f77652c97f871532b1bde696dfaed811
psridevi19 <sridevivcet@gmail.com>
2025-04-07 13:34:33 -0400

    New Commit

--- src/test/java/ecom/testComponents/BaseTest.java
+++ src/test/java/ecom/testComponents/BaseTest.java
@@ -39,7 +39,8 @@ public class BaseTest {
 
         if (browserName.contains("chrome")) {
             ChromeOptions options = new ChromeOptions();
-            if(browserName.contains("headless")){
+            if(browserName.contains("headless"))
+            {
                 options.addArguments("headless");
             }
             driver = new ChromeDriver(options);
